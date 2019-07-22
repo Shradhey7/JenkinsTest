0979936813b1ef9ef7e2bca1bd287eca4a36bbb9
Skali7 <shradheyakumar7@gmail.com>
2019-07-22 18:07:09 +0530

    Adding test file

---
+++ newTest.java
@@ -0,0 +1,12 @@
+public class newTest {
+
+   public static void main(String[] args) {
+
+        for (int i=0;i<5;i++){
+          for (int j=5;j>0;j--){
+            System.out.print("$");
+          }
+        }
+   }
+
+}
